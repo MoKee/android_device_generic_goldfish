5fd92ac1fef239a04e4d6d87a18912ea326277fa
martincz <martincz@mokeedev.com>
2014-02-05 03:57:06 +0800

    mokee bringup
    Change-Id: I8e087c7dfad92c6a9b6441b74f05fc4086c18617

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
 TARGET_KERNEL_SOURCE := kernel/goldfish
-TARGET_KERNEL_CONFIG := cyanogenmod_goldfish_armv7_defconfig
+TARGET_KERNEL_CONFIG := mokee_goldfish_armv7_defconfig
 
 # Inherit MK common GSM stuff.
 $(call inherit-product, vendor/mk/config/gsm.mk)
@@ -13,5 +13,5 @@ $(call inherit-product, device/generic/goldfish/goldfish.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := mk_goldfish
 PRODUCT_DEVICE := generic
-PRODUCT_BRAND := CyanogenMod
+PRODUCT_BRAND := MoKee OpenSource
 PRODUCT_MODEL := Full MoKee OpenSource on Emulator
